683c3c9db0832d47069dba977a88eb646ab00fb3
18820938485 <513631722@qq.com>
2018-01-22 15:05:53 +0800

    first commit

---
+++ HZPlayer/HZPlayer/HZPlayer.h
@@ -0,0 +1,13 @@
+//
+//  HZPlayer.h
+//  HZPlayer
+//
+//  Created by ios开发 on 2017/8/25.
+//  Copyright © 2017年 fuhanzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HZPlayer : UIView
+
+@end
---
+++ HZPlayer/HZPlayer/HZPlayer.m
@@ -0,0 +1,21 @@
+//
+//  HZPlayer.m
+//  HZPlayer
+//
+//  Created by ios开发 on 2017/8/25.
+//  Copyright © 2017年 fuhanzhang. All rights reserved.
+//
+
+#import "HZPlayer.h"
+
+@implementation HZPlayer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
